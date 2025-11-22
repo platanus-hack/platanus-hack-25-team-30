5148f8755aea4383eb969536a8c20056a5ffa9ad
Nicolas Buzeta <nicobuzeta@Nicolass-MBP-2.lan>
2025-11-22 16:42:07 -0300

    add cors

--- platanus-backend/app/main.py
+++ platanus-backend/app/main.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 
 from .db import TORTOISE_ORM
@@ -15,6 +16,18 @@ logging.basicConfig(
 
 app = FastAPI(dependencies=[Depends(get_user_token_header)])
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(persons.router)
